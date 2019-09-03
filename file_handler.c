47f0c1c05715c2da422d28dbc7c3f16b5bb9a07b
Rinkesh Sante <52735633+rinkeshsante@users.noreply.github.com>
2019-09-03 21:22:27 +0530

    Add files via upload

--- file_handler.c
+++ file_handler.c
@@ -1,67 +1,89 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
 
-struct inv
+struct node
 {
 	char name[20];
 	int price;
 	int quantity;
 	int cost;
-	char expiry[20];
-	int limit;
-	char feedback[20];
-	char category[20];
-
-	struct inv *next;
-	struct inv *prev;
+	//char expiry[20];
+	//int limit;
+	//char feedback[20];
+	//char category[20];
+	struct node *next;
+	struct node *prev;
 };
-
-int n=0;
-struct inv *data;
-
-void data_writer(struct inv* node);
-// void data_reader();
-void append(struct inv** head_ref);
-void printList(struct inv* node);
-int counter(struct inv* node);
+struct node *start=NULL;
+struct node *end=NULL;
+void data_writer();
+void data_reader();
+void append();
+void printList();
+int counter();
 
 void main()
 {
-
-	struct inv* head = NULL; 
-	append(&head);	
-	append(&head);
-	printList(head);
-	n = counter(head);
-	data_writer(head);
+	int choice,flag=0;
+	do
+	 {
+	 	printf("1:add new element \n");
+		printf("2:print the liast\n");
+		printf("3:sort the list\n");
+		printf("4:scan present inventary\n");
+		printf("5:exit & save\n");
+		printf("eneter the choice------");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+			append();
+			break;
+			case 2:
+			printList();
+			break;
+			case 3:
+			printf("not Written yet\n");
+			break;
+			case 4:
+			if(flag==0)
+			{
+				data_reader();
+				flag=1;
+			}
+			else
+				printf("already scaned\n");
+			break;
+			case 5:
+			printf("programe ended\n");
+			break;
+			default:
+			printf("invalid operation\n");
+		}
+	 } while (choice!=5);
+	data_writer();
 	getch();
 }
 
-void data_writer(struct inv *node)
-{
-	//writing inventory
+void data_writer()
+{	
+	int n = counter();
 	FILE *fp;
 	fp = fopen("inv.txt", "w");
 	if(fp==NULL)
 		printf("File failed to open\n");
 	else
 	{
-		printf("File is now open.\n");
-		// for (int i = 0; i < n; ++i)
-		// {
-		// 	fprintf(fp, "%s %d %d %d\n", (a+i)->name, (a+i)->price, (a+i)->quantity, (a+i)->cost);
-		// }
-		while (node != NULL)
+		struct node*ptr=start;
+		while (ptr!= NULL)
 		{ 
-			fprintf(fp, "%s %d %d %d\n", node->name, node->price, node->quantity, node->cost); 
-			node = node->next; 
+			fprintf(fp, "%s %d %d %d\n",ptr->name,ptr->price,ptr->quantity,ptr->cost); 
+			ptr=ptr->next; 
 		} 
 		printf("Written to file\n");
 	}
 	fclose(fp);
-
-	//writing count of list
 	fp = fopen("num.txt", "w");
 	if(fp==NULL)
 		printf("File failed to open\n");
@@ -71,97 +93,105 @@ void data_writer(struct inv *node)
 	}
 	fclose(fp);
 }
-// void data_reader()
-// {
-// 	FILE *fp;
-// 	struct inv a[2];
-// 	fp = fopen("inv.txt", "r");
-// 		if(fp==NULL)
-// 			printf("File failed to open\n");
-// 		else
-// 		{
-// 			printf("File is now open.\n");
-// 			for (int i = 0; i < 2; ++i)
-// 			{
-// 				fscanf(fp, "%s %d %d %d", a[i].name, &a[i].price, &a[i].quantity, &a[i].cost);
-// 			}
-// 			printf("Read from file\n");
-// 			for (int i = 0; i < 2; ++i)
-// 			{
-// 				printf("%s %d %d %d\n", a[i].name, a[i].price, a[i].quantity, a[i].cost);
-// 			}
-// 		}
-// 	fclose(fp);
-// }
-void append(struct inv** head_ref) 
-{ 
-	/* 1. allocate node */
-	struct inv* new_node = (struct inv*)malloc(sizeof(struct inv)); 
-
-	struct inv* last = *head_ref; /* used in step 5*/
-
-	/* 2. put in the data */
-	// new_node->data = new_data; 
-	printf("Enter name of item\n");
-	scanf("%s", new_node->name);
-	printf("Enter price\n");
-	scanf("%d", &(new_node->price));
-	printf("Enter quantity\n");
-	scanf("%d", &(new_node->quantity));
-
-	new_node->cost = new_node->price * new_node->quantity;
-	
-	printf("Added:\n");
-
-	printf("%s %d %d %d\n\n", new_node->name, new_node->price, new_node->quantity, new_node->cost);
-
-	/* 3. This new node is going to be the last node, so 
-		make next of it as NULL*/
-	new_node->next = NULL; 
-
-	/* 4. If the Linked List is empty, then make the new 
-		node as head */
-	if (*head_ref == NULL) { 
-		new_node->prev = NULL; 
-		*head_ref = new_node; 
-		return; 
-	} 
-
-	/* 5. Else traverse till the last node */
-	while (last->next != NULL) 
-		last = last->next; 
-
-	/* 6. Change the next of last node */
-	last->next = new_node; 
-
-	/* 7. Make last node as previous of new node */
-	new_node->prev = last; 
-
-	return; 
-} 
-void printList(struct inv* node) 
-{ 
-	struct inv* last; 
-	printf("Traversal in forward direction \n"); 
-	while (node != NULL) { 
-		printf("%s %d %d %d\n", node->name, node->price, node->quantity, node->cost); 
-		last = node; 
-		node = node->next; 
-	} 
-
-	printf("\nTraversal in reverse direction \n"); 
-	while (last != NULL) { 
-		printf("%s %d %d %d\n", last->name, last->price, last->quantity, last->cost); 
-		last = last->prev; 
-	} 
-} 
-int counter(struct inv* node)
+void data_reader()
 {
+ 	int n;
+ 	FILE *fp;
+ 	fp=fopen("num.txt","r");
+ 	if (fp==NULL)
+ 	{
+ 		printf("file failed to open\n");
+ 		exit(0);
+ 	}
+ 	else
+ 	{
+ 		fscanf(fp,"%d",&n);
+ 	}
+ 	fclose(fp);
+
+
+ 	fp= fopen("inv.txt","r");
+ 		if(fp==NULL)
+ 			printf("File failed to open\n");
+ 		else
+ 		{
+ 			for (int i = 0; i <n; ++i)
+ 			{
+ 				struct node *new_node;
+ 				new_node=(struct node*)malloc(sizeof(struct node));
+ 				fscanf(fp, "%s %d %d %d",&new_node->name,&new_node->price
+ 					,&new_node->quantity,&new_node->cost);
+ 				new_node->next=NULL;
+ 				if (start == NULL) 
+				{ 
+					new_node->prev = NULL; 
+					start = new_node;
+					end=new_node; 
+				} 
+				else
+				{
+					end->next = new_node; 
+					new_node->prev =end; 
+					end=new_node;
+				}
+			}
+			printf("%d elemets are added\n", n);
+		}
+ 	fclose(fp);	
+}
+void append() 
+{ 
+	struct node* new_node = (struct node*)malloc(sizeof(struct node)); 
+	printf("Enter name of item : ");
+	scanf("%s", new_node->name);
+	printf("Enter price : ");
+	scanf("%d", &(new_node->price));
+	printf("Enter quantity : ");
+	scanf("%d", &(new_node->quantity));
+	new_node->cost = new_node->price * new_node->quantity;
+	printf("elemets :\t\t\t%s %d %d %d\n", new_node->name, new_node->price, new_node->quantity, new_node->cost);
+	new_node->next = NULL; 
+	if (start == NULL) 
+	{ 
+		new_node->prev = NULL; 
+		start = new_node; 
+		end=new_node;
+	} 
+	else
+	{
+		end->next = new_node; 
+		new_node->prev =end; 
+		end=new_node;
+	}	
+}
+
+
+void printList() 
+{ 
+	struct node*ptr; 
+	printf("Traversal in forward direction \n"); 
+	ptr=start;
+	while (ptr!= NULL) 
+	{ 
+		printf("\t\t\t\t%s %d %d %d\n",ptr->name,ptr->price,ptr->quantity,ptr->cost);
+		ptr= ptr->next; 
+	} 
+/*	ptr=end;
+	printf("\nTraversal in reverse direction \n");
+	while (ptr != NULL) { 
+		printf("%s %d %d %d\n",ptr->name, last->price, ptr->quantity, ptr->cost); 
+		ptr = ptr->prev; 
+	} */
+} 
+
+int counter()
+{
+	struct node*ptr=start;
 	int count=0;
-	while(node!=NULL)
+	while(ptr!=NULL)
 	{
 		count++;
-		node = node->next;
+		ptr=ptr->next;
 	}
 	return count;
 }
